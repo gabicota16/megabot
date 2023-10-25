2d3ed6b109805974cfc376e8395d5d26b07c04b6
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2023-10-25 14:16:41 -0300

    Mega

--- index.js
+++ index.js
@@ -1,80 +1,70 @@
-console.log('✅ㅤIniciando...');
-import {join, dirname} from 'path';
-import {createRequire} from 'module';
-import {fileURLToPath} from 'url';
-import {setupMaster, fork} from 'cluster';
-import {watchFile, unwatchFile} from 'fs';
-import cfonts from 'cfonts';
-import {createInterface} from 'readline';
-import yargs from 'yargs';
+import { join, dirname } from "path";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { setupMaster, fork } from "cluster";
+import { watchFile, unwatchFile } from "fs";
+import cfonts from "cfonts";
+import chalk from "chalk";
+import util from "util"
+import { createInterface } from "readline";
+import yargs from "yargs";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const {name, author} = require(join(__dirname, './package.json'));
-const {say} = cfonts;
+const { name, author } = require(join(__dirname, "./package.json"));
+const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']});
+say('Mega\nBot', {
+font: 'chrome',
+align: 'center',
+gradient: ['red', 'magenta']})
+say(`Project Author:\nGabriel (@gabiacosta06)`, {
+font: 'console',
+align: 'center',
+gradient: ['red', 'magenta']});
 
-let isRunning = false;
+var isRunning = false;
 /**
-* Start a js file
+* Start a js file 
 * @param {String} file `path/to/file`
 */
 function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
-
-  say('Ajuste la pantalla para escanear el codigo QR', {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']});
-
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1)});
-  const p = fork();
-  p.on('message', (data) => {
-    console.log('[RECEIVED]', data);
-    switch (data) {
-      case 'reset':
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-    }
-  });
-  p.on('exit', (_, code) => {
-    isRunning = false;
-    console.error('❎ㅤOcurrio un error inesperado:', code);
-
-    p.process.kill();
-    isRunning = false;
-    start.apply(this, arguments);
-
-    if (process.env.pm_id) {
-      process.exit(1);
-    } else {
-      process.exit();
-    }
-  });
-  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-  if (!opts['test']) {
-    if (!rl.listenerCount()) {
-      rl.on('line', (line) => {
-        p.emit('message', line.trim());
-      });
-    }
-  }
+if (isRunning) return;
+isRunning = true;
+let args = [join(__dirname, file), ...process.argv.slice(2)];
+  
+setupMaster({
+exec: args[0],
+args: args.slice(1),
+});
+let p = fork();
+p.on('message', data => {
+//console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
+switch (data) {
+case 'reset':
+p.process.kill();
+isRunning = false;
+start.apply(this, arguments);
+break;
+case 'uptime':
+p.send(process.uptime());
+break;
+}});
+p.on('exit', (_, code) => {
+isRunning = false;
+console.error('⚠️ ERROR ⚠️', code);
+if (code === 0) return;
+watchFile(args[0], () => {
+unwatchFile(args[0]);
+start(file);
+});
+});
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+if (!opts['test']);
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim());
+});
+//console.log(p)
 }
+
 start('main.js');
